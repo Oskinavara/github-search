c3573c5b83fb62e7adb0a2ebe304857fa3b9acb8
Oskar <oskar.nawara@monterail.com>
2021-07-25 14:23:11 +0200

    Prepare to deploy

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
-import Input from 'src/components/atoms/Input';
 
 test('Renders the app', () => {
   const { getByText } = render(
@@ -13,8 +12,3 @@ test('Renders the app', () => {
 
   expect(getByText('Search')).toBeInTheDocument();
 });
-
-// test('renders component', async () => {
-//   const { getByText } = render(<App />);
-//   expect(getByText('Please specify value')).toBeInTheDocument();
-// });
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axiosInstance from 'axios';
 axiosInstance.defaults.baseURL = 'https://api.github.com/search/users';
 axiosInstance.defaults.headers.get = {
   'Content-Type': 'application/json',
-  Authorization: 'Bearer ' + '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',
+  Authorization: 'Bearer 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',
 };
 
 export default axiosInstance;
