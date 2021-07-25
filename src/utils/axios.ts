75cfaf7c2687f3b3ba6af83cb4212734d51610f2
Oskar <oskar.nawara@monterail.com>
2021-07-25 16:36:04 +0200

    Remove github token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,6 @@ import axiosInstance from 'axios';
 axiosInstance.defaults.baseURL = 'https://api.github.com/search/users';
 axiosInstance.defaults.headers.get = {
   'Content-Type': 'application/json',
-  Authorization: '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',
 };
 
 export default axiosInstance;
