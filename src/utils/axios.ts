b56bef137f5cbfdf535482f161b4b44ba85f3ec0
Oskar <oskar.nawara@monterail.com>
2021-07-25 14:51:06 +0200

    Push the api key

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,11 +1,9 @@
 import axiosInstance from 'axios';
-const API_KEY = process.env.API_KEY;
-const authHeader = API_KEY ? `Bearer ${API_KEY}` : '';
 
 axiosInstance.defaults.baseURL = 'https://api.github.com/search/users';
 axiosInstance.defaults.headers.get = {
   'Content-Type': 'application/json',
-  Authorization: authHeader,
+  Authorization: process.env.API_KEY,
 };
 
 export default axiosInstance;
